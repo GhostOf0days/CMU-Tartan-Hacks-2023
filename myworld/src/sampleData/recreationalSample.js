c9a2abcda09d1b5c8c90319cded0fa5c65b5e02d
SapheSaber <aac2@andrew.cmu.edu>
2023-02-03 23:41:53 -0500

    imported map image and sample data file

---
+++ myworld/src/sampleData/recreationalSample.js
@@ -0,0 +1,24 @@
+const recreationalSample = [
+    {
+        id: 1,
+        title: "Local Basketball events",
+        date: "18 March 2023",
+        location: "1082 Shady Ave, Pittsburgh, Pennsylvania",
+        type: "sports",
+        desc: "A local basketball tournament happening soon! All genders welcome! (age 16 and above)"
+    },
+    {
+        id: 2,
+        title: "Singing competition",
+        date: "18 March 2023",
+        location: "1082 Shady Ave, Pittsburgh, Pennsylvania",
+        type: "sports",
+        desc: "A local basketball tournament happening soon! All genders welcome! (age 16 and above)"
+    },
+
+
+
+
+]
+
+export default recreationalSample;
